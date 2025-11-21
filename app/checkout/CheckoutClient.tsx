fc7e49800b790c025a45010ed6d13d3463b1db99
amro22397 <amroalmutasim22@gmail.com>
2025-11-21 13:22:17 +0400

    many changes 21-NOV-2025 1322

--- app/checkout/CheckoutClient.tsx
+++ app/checkout/CheckoutClient.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import Script from "next/script";
+
 // import { useCart } from '@/hooks/useCart';
 // import { Elements } from '@stripe/react-stripe-js';
 // import { loadStripe, StripeElementsOptions } from '@stripe/stripe-js';
 // import { useRouter } from "next/navigation";
 import React, { useState } from "react";
-import Script from "next/script";
 
 // import toast from 'react-hot-toast';
 // import CheckoutForm from './CheckoutForm';
@@ -25,52 +26,40 @@ const CheckoutClient = ({ user }: { user: User }) => {
 
   const [msg, setMsg] = useState("");
 
-
   // const [testData, setTestData] = useState({ amount: 1, customer_email: user?.email, customer_name: user?.name });
   // const [error, setError] = useState(false);
   // const [clientSecret, setClientSecret] = useState("");
   // const [paymentSuccess, setPaymentSuccess] = useState(false);
 
-
   const [data, setData] = useState();
   // const router = useRouter();
-
-
 
   const handleCheckout = async () => {
     setLoading(true);
 
     // 1️⃣ Call backend to create a session
-    const res = await fetch('/api/checkout/session', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ amount: 100, currency: 'USD' }),
+    const res = await fetch("/api/checkout/session", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ amount: 100, currency: "USD" }),
       // change the amount here >> 1$ = 100 centes (amount must be in cents)
     });
 
     const data = await res.json();
 
-    
-
     if (data.session && data.session.id) {
       // 2️⃣ Redirect user to Checkout.com's hosted payment page
       window.location.href = data.session.redirect_url;
     } else {
-      setResponse(data)
-      alert('Error creating checkout session');
+      setResponse(data);
+      alert("Error creating checkout session");
       console.error(`Error creating checkout session: ${data.error}`);
     }
 
     setLoading(false);
   };
 
-
-
-
-
-
   // paytabs payments
-
 
   const handlePayment = async () => {
     setLoading(true);
@@ -78,11 +67,15 @@ const CheckoutClient = ({ user }: { user: User }) => {
       const res = await fetch("/api/paytabs", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ amount: 1, customer_email: user?.email, customer_name: user?.name }),
+        body: JSON.stringify({
+          amount: 1,
+          customer_email: user?.email,
+          customer_name: user?.name,
+        }),
       });
       const data = await res.json();
 
-      setData(data)
+      setData(data);
 
       if (data.payment_url) {
         window.location.href = data.payment_url; // redirect to PayTabs payment page
@@ -97,20 +90,14 @@ const CheckoutClient = ({ user }: { user: User }) => {
     }
   };
 
-
-
-
-
   // tap payments
-
 
   const handlePay = async () => {
     setLoading(true);
 
     if (!user) {
-      alert("Please login first")
+      alert("Please login first");
     }
-
 
     const res = await fetch("/api/create-payment", {
       method: "POST",
@@ -126,15 +113,12 @@ const CheckoutClient = ({ user }: { user: User }) => {
     setLoading(false);
   };
 
-
-
-
-
   // 2Checkout payment
-
 
   const pay = () => {
     setLoading(true);
+
+    console.log(window.TCO)
 
     if (!window.TCO) {
       setMsg("TCO library not loaded!");
@@ -143,60 +127,56 @@ const CheckoutClient = ({ user }: { user: User }) => {
     }
 
     try {
-      
-
       const args = {
-      sellerId: process.env.NEXT_PUBLIC_TWOCHECKOUT_SELLER_ID,
-      publishableKey: process.env.NEXT_PUBLIC_TWOCHECKOUT_PUBLIC_KEY,
-      ccNo: document.getElementById("ccNo").value,
-      cvv: document.getElementById("cvv").value,
-      expMonth: document.getElementById("expMonth").value,
-      expYear: document.getElementById("expYear").value,
-    };
+        sellerId: process.env.NEXT_PUBLIC_TWOCHECKOUT_SELLER_ID,
+        publishableKey: process.env.NEXT_PUBLIC_TWOCHECKOUT_PUBLIC_KEY,
+        ccNo: document.getElementById("ccNo").value,
+        cvv: document.getElementById("cvv").value,
+        expMonth: document.getElementById("expMonth").value,
+        expYear: document.getElementById("expYear").value,
+      };
 
-    window.TCO.loadPubKey("sandbox", async function () {
-      window.TCO.requestToken(async function (data: any) {
-        const res = await fetch("/api/2checkout/pay", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            token: data.response.token.token,
-            amount: 1, // it was 50
-          }),
-        });
+      window.TCO.loadPubKey("sandbox", async function () {
+        window.TCO.requestToken(
+          async function (data: any) {
+            const res = await fetch("/api/2checkout/pay", {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({
+                token: data.response.token.token,
+                amount: 1, // it was 50
+              }),
+            });
 
-        const json = await res.json();
-        setMsg(json.message);
-        setLoading(false);
-      },
-      function (error) {
-        setMsg(error.errorMsg);
-        setLoading(false);
-      }, args);
-    });
+            const json = await res.json();
+            setMsg(json.message);
+            setLoading(false);
+          },
+          function (error) {
+            setMsg(error.errorMsg);
+            setLoading(false);
+          },
+          args
+        );
+      });
 
-    setLoading(false);
-
-
+      setLoading(false);
     } catch (error) {
-      
-      console.log(`Client error with 2Checkout payment: ${error}`)
+      console.log(`Client error with 2Checkout payment: ${error}`);
 
-      setMsg(`Client error with 2Checkout payment: ${error}`)
+      setMsg(`Client error with 2Checkout payment: ${error}`);
 
-      alert(`Client error with 2Checkout payment: ${error}`)
+      alert(`Client error with 2Checkout payment: ${error}`);
 
       setLoading(false);
     }
   };
 
-
-
   const isRunning = false;
 
   if (isRunning) {
     handleCheckout();
-    handlePay()
+    handlePay();
     handlePayment();
   }
 
@@ -257,21 +237,25 @@ useEffect(() => {
             I make Stripe Account...
         </span> */}
 
-        <Script src="https://secure.2checkout.com/checkout/api/2co.min.js"
+      <Script
+        src="https://secure.2checkout.com/checkout/api/2co.min.js"
         strategy="beforeInteractive"
-        />
+      />
 
-        <pre className="">{JSON.stringify(data, null, 2)}</pre>
+      <pre className="">{JSON.stringify(data, null, 2)}</pre>
 
       <h1 className="text-2xl">Checkout Page</h1>
 
-
       <h1>2Checkout Payment</h1>
 
-      <input id="ccNo" placeholder="Card Number" /><br />
-      <input id="cvv" placeholder="CVV" /><br />
-      <input id="expMonth" placeholder="Exp Month" /><br />
-      <input id="expYear" placeholder="Exp Year" /><br />
+      <input id="ccNo" placeholder="Card Number" />
+      <br />
+      <input id="cvv" placeholder="CVV" />
+      <br />
+      <input id="expMonth" placeholder="Exp Month" />
+      <br />
+      <input id="expYear" placeholder="Exp Year" />
+      <br />
 
       <button onClick={pay} disabled={loading}>
         {loading ? "Processing..." : "Pay 1$"}
@@ -279,12 +263,10 @@ useEffect(() => {
 
       <p>{msg}</p>
 
-
       <span className="text-center text-rose-600">
-            You may be able to pay with (Apple Pay, Google Pay), but I&apos;m working to make
-            debit card payments available.
-        </span>
-
+        You may be able to pay with (Apple Pay, Google Pay), but I&apos;m
+        working to make debit card payments available.
+      </span>
 
       <button
         onClick={handlePay}
@@ -297,11 +279,11 @@ useEffect(() => {
       <div className="flex flex-row gap-[14px] items-center justify-end mt-3">
         {paymentTypesImages.map((item, index) => (
           <Image
-          key={index}
-          src={item.image}
-          alt={item.name}
-          width={50}
-          height={50}
+            key={index}
+            src={item.image}
+            alt={item.name}
+            width={50}
+            height={50}
           />
         ))}
       </div>
